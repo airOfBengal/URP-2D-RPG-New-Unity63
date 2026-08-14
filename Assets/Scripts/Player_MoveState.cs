b939a8b7ab31eaa7cfbd686f4501453c0eb701ec
Md. Atiqul Islam <air.cse.pbn@gmail.com>
2026-08-14 23:48:22 +0600

    character flip

--- Assets/Scripts/Player_MoveState.cs
+++ Assets/Scripts/Player_MoveState.cs
@@ -10,6 +10,17 @@ public class Player_MoveState : EntityState
     {
         base.Update();        
 
+        // adjust left-right facing
+        if(player.moveInput.x < 0)
+        {
+            player.gameObject.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+        }
+        else if(player.moveInput.x > 0)
+        {
+            player.gameObject.transform.rotation = Quaternion.Euler(Vector3.zero);
+        }
+
+        // add movement
         player.SetVelocity(player.moveInput.x * player.moveSpeed * Time.deltaTime, player.moveInput.y * player.moveSpeed * Time.deltaTime);
     }
 }
