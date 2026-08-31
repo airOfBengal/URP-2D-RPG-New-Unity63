0ae852ae8e86a8eb17062e712e14bb33618d7863
Md. Atiqul Islam <air.cse.pbn@gmail.com>
2026-08-31 19:31:26 +0600

    enemy setup

---
+++ Assets/Scripts/EnemyState.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class EnemyState : EntityState
+{
+    protected Enemy enemy;
+
+    public EnemyState(Enemy enemy, StateMachine stateMachine, string stateName) : base(stateMachine, stateName)
+    {
+        this.enemy = enemy;
+        rb = enemy.rb;
+        anim = enemy.anim;
+    }
+}
